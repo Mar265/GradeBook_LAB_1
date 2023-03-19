0311c0bf40332b4226d8a56313b9734ba4b820e8
Marcin Góral <marcin.goral@microsoft.wsei.edu.pl>
2023-03-19 20:36:04 +0100

    12/12

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,6 +29,18 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+            
+            if (this.Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics(); 
+            }
+        }
 
     }
 }
