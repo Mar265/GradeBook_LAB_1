1bf3c704f6f1fca11d54729f535cc83e1d655c45
Marcin Góral <marcin.goral@microsoft.wsei.edu.pl>
2023-03-19 20:17:11 +0100

    Class & Override

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,34 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+
+        }
+        public override char GetLetterGrade ( double averageGrade)
+        {
+            if (this.Students.Count < 5)
+                throw new InvalidOperationException();
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
+
+    }
+}
